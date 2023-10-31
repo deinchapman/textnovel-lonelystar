9a466453be2c31bf0982e45fd9b07dfa9d2ece4e
Vaibhav Jain <jainvaibhav671@gmail.com>
2023-10-31 10:30:24 +0530

    Added Multiline strings

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-intro = ("\nOnce again,\nyou,\nthe \"Mortificado\",\na being that has experience nothing else but suffer in his life,\nhas once again experienced a new calamity.\nAt this point...\nyou just want to go to your especial place;\n...and talk with the only being that you feel that listens to you...\n")
+intro = """
+Once again,
+you,
+the "Mortificado",
+a being that has experience nothing else but suffer in his life,
+has once again experienced a new calamity.
+At this point...
+you just want to go to your especial place;
+...and talk with the only being that you feel that listens to you...
+"""
 
-text1v1 = ("\nToday you went out to look for the moon as you always do.\nYou only wish to the least being able to delight yourself with it's beauty.\n")
+text1v1 = """
+Today you went out to look for the moon as you always do.
+You only wish to the least being able to delight yourself with it's beauty.
+"""
 
-text2v1 = ("\nWhile you try to look at the sky,\nit seems that you cannot be able to find the moon by nowhere you try to look at;\nnevertheless... the one thing you could find is a lonely star,\nthat only because of it's lonliness you let yourself notice it's poor shine.\n")
+text2v1 = """
+While you try to look at the sky,
+it seems that you cannot be able to find the moon by nowhere you try to look at;
+nevertheless... the one thing you could find is a lonely star,
+that only because of it's lonliness you let yourself notice it's poor shine.
+"""
 
-endingSimp = ("\nYou just ignored the star,\nand went on to keep looking for the moon,\nno matter how much it does cost you to find it,\neven if you've lost yourself,\nyou'll keep chase her for forever.\n \n \n Ending S: [S]imp walking")
+endingSimp = """
+You just ignored the star,
+and went on to keep looking for the moon,
+no matter how much it does cost you to find it,
+even if you've lost yourself,
+you'll keep chase her for forever.
 
-text1v2 = ('\nThe Lonely Star answers to your shouts with another question: \n- I\'m the one who wants to ask,\n"Moritificado";\nIs it worth go looking for it? -\n')
-               
-text2v2 = ("\n- Just try to call and shout her name,\nI just wanna let you know that if nobody answers,\nit's because nobody cares -\n")
 
-text3v2 = ("\n- If you ask \"me\" a lonely star,\nThe moon didn't gave you anything back.\nThe moon is not worth the effort it's my conclusion,\n\"Mortificado\" my friend...\nNever mind it's beauty,\nif you were to go chasing behind her,\nyou'd be at garbage time -\n \n \n Ending R: [R]eality Check")
+Ending S: [S]imp walking
+"""
+
+text1v2 = """
+The Lonely Star answers to your shouts with another question: 
+- I'm the one who wants to ask,"Moritificado";
+Is it worth go looking for it? -
+"""
+
+text2v2 = """
+- Just try to call and shout her name,
+I just wanna let you know that if nobody answers,
+it's because nobody cares -")
+"""
+
+text3v2 = """
+- If you ask "me" a lonely star,
+The moon didn't gave you anything back.
+The moon is not worth the effort it's my conclusion,
+"Mortificado" my friend...
+Never mind it's beauty,
+if you were to go chasing behind her,
+you'd be at garbage time -
+
+
+Ending R: [R]eality Check"
+"""
 
 print(intro)
 print()
@@ -54,15 +99,3 @@ if(1):
     print(text3v2)
 else:
     print(text3v2)
-
-
-    
-    
-
-    
-    
-    
-    
-    
-
-
